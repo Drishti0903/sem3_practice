fbd2118aea0d9bbd1e938bbdbfa54b69eff04d36
Drishti0903 <114413546+Drishti0903@users.noreply.github.com>
2022-11-02 11:55:08 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,56 @@
+# letters=['a','b','d','e','i','j','o']
+# def filter_vowels(letters):
+#     vowels=['a','e','i','o','u']
+#     return True if letters in vowels else False
+# filtered_vowels=filter(filter_vowels,letters)
+# vowels=tuple(filtered_vowels)
+# print(vowels)
+
+
+# numbers=[1,2,3,4,5,6,7]
+# even_numbers=filter(lambda x:(x%2==0),numbers)
+# even=list(even_numbers)
+# print(even)
+
+
+# from tkinter import*
+# from tkinter import messagebox
+# top=Tk()
+# top.geometry("200x100")
+
+# def fun():
+#     messagebox.showinfo("hello","red button clicked")
+
+# b1=Button(top,text="red",command=fun,activeforeground="red",activebackground="pink",pady=10)
+# b2=Button(top,text="blue",activeforeground="blue",activebackground="pink",pady=10)
+# b3=Button(top,text="green",activeforeground="green",activebackground="pink",pady=10)
+# b4=Button(top,text="yellow",activeforeground="yellow",activebackground="pink",pady=10)
+
+# b1.pack(side=LEFT)
+# b2.pack(side=RIGHT)
+# b3.pack(side=TOP)
+# b4.pack(side=BOTTOM)
+# top.mainloop()
+
+
+# from tkinter import*
+# pane=Tk()
+# b1=Button(pane,text="click me")
+# b1.pack(fill=Y,expand=True)
+# b2=Button(pane,text="click me too")
+# b2.pack(fill=X,expand=True)
+# pane.mainloop()
+
+
+
+
+# from tkinter import*
+# m=Tk()
+# m.geometry("500x500")
+# b1=Button(m,text="submit")
+# b1.place(anchor=NE,relx=1,x=-2,y=2)
+# b2=Button(m,text="new")
+# b2.place(anchor=CENTER,relx=0.5,rely=0.5)
+# m.mainloop()
+
+
